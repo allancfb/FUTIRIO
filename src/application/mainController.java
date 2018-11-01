e623e176b752dcf588bfb578a4b5aa74f180b7d3
Henrique Filho <hbaf71@gmail.com>
2018-11-01 17:39:47 -0300

    Adjustments

--- src/application/mainController.java
+++ src/application/mainController.java
@@ -3,22 +3,16 @@ package application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 
 import java.awt.Button;
 import java.net.URL;
 import java.util.*;
 
+import org.omg.CORBA.portable.IndirectionException;
+
 public class mainController implements Initializable{
-	
-	@FXML
-	private Button jogar;
-	
-	@FXML
-	private Button creditos;
-	
-	@FXML
-	private Button sair;
 	
 	public void jogar(ActionEvent event) {
 		//TODO vai pra cena para escolher características do jogo futuro
