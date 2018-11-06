663deeaf0a8aa1af1120b7a3106caae4e8cc48da
Henrique Filho <hbaf71@gmail.com>
2018-11-06 15:41:02 -0200

    layout changes

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,7 +17,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
+
 	@FXML
 	private Button jogar;
 
@@ -26,16 +26,16 @@ public class Main extends Application {
 
 	@FXML
 	private Button sair;
-	
+
 	@FXML
 	private Button voltar;
-	
+
 	Stage stage = new Stage();
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		try {
@@ -43,7 +43,7 @@ public class Main extends Application {
 			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
 
 			Scene inicio = new Scene(root);
-			
+
 			primaryStage.setTitle("FUTIRIO");
 			primaryStage.setScene(inicio);
 			primaryStage.show();
@@ -51,13 +51,14 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
-	//CENA DE INICIO
+
+	// CENA DE INICIO
 	@FXML
 	public void jogar(ActionEvent event) throws IOException {
 		Parent jogarParent = FXMLLoader.load(getClass().getResource("selecionaOpcoes.fxml"));
 		Scene jogar = new Scene(jogarParent);
-		
+
+		stage.setTitle("FUTIRIO");
 		stage.setScene(jogar);
 		stage.show();
 	}
@@ -66,7 +67,8 @@ public class Main extends Application {
 	public void creditos(ActionEvent event) throws IOException {
 		Parent creditosParent = FXMLLoader.load(getClass().getResource("Creditos.fxml"));
 		Scene creditos = new Scene(creditosParent);
-		
+
+		stage.setTitle("FUTIRIO");
 		stage.setScene(creditos);
 		stage.show();
 	}
@@ -75,16 +77,18 @@ public class Main extends Application {
 	public void sair(ActionEvent event) {
 		Platform.exit();
 	}
+
 	//////////////////////////////////////////////////////////////////
-	//Cena de Créditos
+	// Cena de Créditos
 	@FXML
-	public void voltar(ActionEvent event) throws IOException{
+	public void voltar(ActionEvent event) throws IOException {
 		Parent inicioParent = FXMLLoader.load(getClass().getResource("Main.fxml"));
 		Scene inicio = new Scene(inicioParent);
-		
-		//Setando a cena a ser mostrada
+
+		// Setando a cena a ser mostrada
+		stage.setTitle("FUTIRIO");
 		stage.setScene(inicio);
-		stage.show();		
+		stage.show();
 	}
 
 }
