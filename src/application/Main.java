f2043ef2ea7d5b51ed5e008d2e69aca954d96827
Henrique Filho <hbaf71@gmail.com>
2018-11-06 15:16:44 -0200

    adjustments

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
