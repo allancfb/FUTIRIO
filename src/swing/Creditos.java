48d6d924d2919a8193d23e6047fb3503a8a5a5cb
Henrique Filho <hbaf71@gmail.com>
2018-11-08 17:50:29 -0200

    ajustments na tela de créditos

--- src/swing/Creditos.java
+++ src/swing/Creditos.java
@@ -6,6 +6,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Creditos extends JFrame {
 
@@ -33,11 +39,24 @@ public class Creditos extends JFrame {
 	public Creditos() {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 600, 399);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnVoltar = new JButton("Voltar");
+		btnVoltar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showMessageDialog(null, "volta pra cena inicial");
+			}
+		});
+		btnVoltar.setBounds(10, 336, 89, 23);
+		contentPane.add(btnVoltar);
+		
+		JLabel background = new JLabel("");
+		background.setIcon(new ImageIcon("C:\\Users\\hfogo_000\\eclipse\\FutirioSwing\\src\\Images\\creditosBG.png"));
+		background.setBounds(0, 0, 600, 399);
+		contentPane.add(background);
 	}
-
 }
