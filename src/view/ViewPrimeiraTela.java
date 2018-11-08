4c731cf7833c7cf3b8e8c9efd41c675c47d7abe2
Henrique Filho <hbaf71@gmail.com>
2018-11-08 17:17:33 -0200

    LET'S MOVE TO SWING

---
+++ src/view/ViewPrimeiraTela.java
@@ -0,0 +1,88 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import javafx.application.Platform;
+
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Canvas;
+import java.awt.Label;
+import java.awt.Button;
+import javax.swing.ImageIcon;
+
+public class ViewPrimeiraTela extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ViewPrimeiraTela frame = new ViewPrimeiraTela();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ViewPrimeiraTela() {
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 600, 399);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton creditos = new JButton("Cr\u00E9ditos");
+		creditos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "vai pra cena de crédito");
+			}
+		});
+		creditos.setBounds(252, 252, 89, 23);
+		contentPane.add(creditos);
+		
+		JButton sair = new JButton("Sair");
+		sair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		
+		JButton jogar = new JButton("Jogar");
+		jogar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Vai pra cena de jogo");
+			}
+		});
+		jogar.setBounds(252, 200, 89, 23);
+		contentPane.add(jogar);
+		sair.setBounds(252, 299, 89, 23);
+		contentPane.add(sair);
+		
+		JLabel background = new JLabel("New label");
+		background.setIcon(new ImageIcon("C:\\Users\\hfogo_000\\eclipse\\FutirioSwing\\src\\Images\\mainBG.png"));
+		background.setBounds(0, 0, 594, 370);
+		contentPane.add(background);
+	}
+}
