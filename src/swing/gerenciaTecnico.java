c642b27894b6a1dbc1c2e472556b6454a11836bf
Henrique Filho <hbaf71@gmail.com>
2018-11-09 16:56:02 -0200

    error message

--- src/swing/gerenciaTecnico.java
+++ src/swing/gerenciaTecnico.java
@@ -4,6 +4,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 import java.awt.Color;
 import java.awt.Font;
@@ -14,6 +15,7 @@ import javax.swing.SwingConstants;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.DefaultComboBoxModel;
+import java.awt.Choice;
 
 public class gerenciaTecnico {
 
@@ -51,42 +53,47 @@ public class gerenciaTecnico {
 		frame.setBounds(100, 100, 600, 399);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JButton btnAvanar = new JButton("Avan\u00E7ar");
 		btnAvanar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				// TODO se os elementos não tiverem sido selecionados, mostra mensagem:
+				// Selecione todas as opções para continuar
+				/* if(elementos não selecionados) */
+				JOptionPane.showMessageDialog(frame, "Selecione todas as opções para continuar", "Erro",
+						JOptionPane.ERROR_MESSAGE);
 			}
 		});
 		btnAvanar.setBounds(247, 270, 89, 23);
 		frame.getContentPane().add(btnAvanar);
-		
+
 		JLabel lblTimes = new JLabel("Selecione o time que deseja comandar:");
 		lblTimes.setForeground(Color.WHITE);
 		lblTimes.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblTimes.setBounds(120, 165, 370, 27);
 		frame.getContentPane().add(lblTimes);
-		
+
 		JComboBox times = new JComboBox();
-		times.setModel(new DefaultComboBoxModel(new String[] {"Botafogo", "Flamengo", "Fluminense", "Vasco"}));
+		times.setModel(new DefaultComboBoxModel(new String[] { "Botafogo", "Flamengo", "Fluminense", "Vasco" }));
 		times.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		times.setToolTipText("Times");
 		times.setBounds(120, 193, 117, 31);
 		frame.getContentPane().add(times);
-		
+
 		nomeTecnico = new JTextField();
+		nomeTecnico.setToolTipText("");
 		nomeTecnico.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		nomeTecnico.setForeground(Color.LIGHT_GRAY);
-		nomeTecnico.setText("Nome do t\u00E9cnico");
+		nomeTecnico.setForeground(Color.BLACK);
 		nomeTecnico.setBounds(120, 108, 259, 27);
 		frame.getContentPane().add(nomeTecnico);
 		nomeTecnico.setColumns(10);
-		
+
 		JLabel lblNome = new JLabel("Insira o nome do t\u00E9cnico:");
 		lblNome.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblNome.setForeground(Color.WHITE);
 		lblNome.setBounds(120, 63, 259, 47);
 		frame.getContentPane().add(lblNome);
-		
+
 		JLabel background = new JLabel("New label");
 		background.setIcon(new ImageIcon(gerenciaTecnico.class.getResource("/Imagens/campo.jpg")));
 		background.setBounds(0, 0, 594, 370);
