c87dee869fe8074e1c7d201f07030cd7624719b6
JoãoVictor <joao.figueira@uniriotec.br>
2018-11-13 16:16:44 -0200

    TimeTablePronto

--- src/entity/TimeTabela.java
+++ src/entity/TimeTabela.java
@@ -11,6 +11,21 @@ public class TimeTabela {
 	private int derrotas;
 	private int golsSofridos;
 
+	//Construtor Definitivo
+	TimeTabela(Time time){
+		
+		this.time = time;
+		this.pontos = 0;
+		this.vitorias = 0;
+		this.saldoGols = 0;
+		this.qtGols = 0;
+		this.empates = 0;
+		this.derrotas = 0;
+		this.golsSofridos = 0;
+	
+	}
+	
+	//Construtor Teste (DEPOIS DE TESTAR PODE APAGAR)
 	TimeTabela(Time time, int pontos, int vitorias, int saldoGols, int qtdGols, int empates, int derrotas, int golSofrido){
 		
 		this.time = time;
@@ -57,5 +72,36 @@ public class TimeTabela {
 		return saldoGols;
 	}
 
-	
+	//Adiciona Vitoria
+	public void addVitoria() {
+		this.vitorias += 1;
+		this.pontos += 3;
+	}
+
+	//Atualiza o saldo de gols
+	public void attSaldoGols(int gols) {
+		this.saldoGols = this.qtGols - this.golsSofridos;
+	}
+
+	//Adiciona qtdGols
+	public void addQtGols(int Gols) {
+		this.qtGols += Gols;
+	}
+
+	//Adiciona empate
+	public void addEmpate() {
+		this.empates += 1;
+		this.pontos += 1;
+	}
+
+	//Adiciona derrota
+	public void addDerrota() {
+		this.derrotas += 1;
+	}
+
+	//Adicionar os gols sofridos no ultimo jogo
+	public void addGolsSofridos(int golsSofridos) {
+		this.golsSofridos += golsSofridos;
+	}
+
 }
