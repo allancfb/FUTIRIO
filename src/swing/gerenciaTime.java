40aaed97521d4b1111cb07109e216f9bf34479b8
Henrique Filho <hbaf71@gmail.com>
2018-11-20 21:28:46 -0200

    atualizachanges

--- src/swing/gerenciaTime.java
+++ src/swing/gerenciaTime.java
@@ -19,6 +19,8 @@ import javax.swing.ListSelectionModel;
 import javax.swing.JMenu;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
 
 public class gerenciaTime extends JFrame {
 
@@ -39,7 +41,7 @@ public class gerenciaTime extends JFrame {
 
 	public gerenciaTime(String nome, String tecnico, int index) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 687, 496);
+		setBounds(100, 100, 634, 496);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -52,12 +54,21 @@ public class gerenciaTime extends JFrame {
 				index = getTime.getIndex();
 			}
 		}
+		//pegando o time adversário
+		int intAdver = 3; //número do adversário
+		String nomeAdver = new String("");
+		for (Time getTime : Time.values()) {
+			if (getTime.getIndex() == intAdver) {
+				nomeAdver = getTime.name().toString();
+				intAdver = getTime.getIndex();
+			}
+		}
 		
 		JLabel escudoTime = new JLabel();
 		Image escudo = new ImageIcon(this.getClass().getResource("/Imagens/"+Integer.toString(index)+".png")).getImage();
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(31, 132, 440, 302);
+		scrollPane.setBounds(31, 132, 457, 302);
 		contentPane.add(scrollPane);
 		
 		jogadores = new JTable();
@@ -68,16 +79,61 @@ public class gerenciaTime extends JFrame {
 
 		JLabel nomeTime = new JLabel(nome);
 		nomeTime.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		nomeTime.setBounds(152, 11, 394, 33);
+		nomeTime.setBounds(152, 11, 336, 33);
 		contentPane.add(nomeTime);
 
 		JLabel nomeTecnico = new JLabel("Técnico: " + tecnico);
 		nomeTecnico.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		nomeTecnico.setBounds(152, 55, 394, 33);
+		nomeTecnico.setBounds(152, 50, 336, 33);
 		contentPane.add(nomeTecnico);
 		
-		JLabel label = new JLabel("");
-		label.setBounds(0, 0, 687, 457);
-		contentPane.add(label);
+		JLabel lblPrximoJogo = new JLabel("Pr\u00F3ximo jogo:");
+		lblPrximoJogo.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblPrximoJogo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPrximoJogo.setBounds(498, 133, 110, 23);
+		contentPane.add(lblPrximoJogo);
+		
+		//Dados do Adversário
+		
+		JLabel escudoAdversario = new JLabel("");
+		escudoAdversario.setBounds(498, 201, 110, 110);
+		Image escudoAdver = new ImageIcon(this.getClass().getResource("/Imagens/"+Integer.toString(intAdver)+".png")).getImage(); 
+		escudoAdversario.setIcon(new ImageIcon(escudoAdver));
+		contentPane.add(escudoAdversario);
+		
+		JLabel nomeAdversario = new JLabel(nomeAdver);
+		nomeAdversario.setText(nomeAdver);
+		nomeAdversario.setHorizontalAlignment(SwingConstants.CENTER);
+		nomeAdversario.setBounds(498, 322, 110, 23);
+		contentPane.add(nomeAdversario);
+		
+		JLabel lblPosio = new JLabel("Posi\u00E7\u00E3o:");
+		lblPosio.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPosio.setBounds(498, 356, 110, 23);
+		contentPane.add(lblPosio);
+		
+		////////////////////////////////////////////////////////////////////////////////////////////
+		
+		JLabel lblCampeonato = new JLabel("Campeonato");
+		lblCampeonato.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCampeonato.setBounds(498, 167, 110, 23);
+		contentPane.add(lblCampeonato);
+		
+		JButton btnJogar = new JButton("Jogar");
+		btnJogar.setBounds(508, 390, 89, 23);
+		contentPane.add(btnJogar);
+		
+		JButton btnTabela = new JButton("Tabela");
+		btnTabela.setBounds(508, 20, 89, 23);
+		contentPane.add(btnTabela);
+		
+		JLabel lblNewLabel = new JLabel("Posi\u00E7\u00E3o: ");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel.setBounds(152, 94, 336, 27);
+		contentPane.add(lblNewLabel);
+		
+		JLabel bg = new JLabel("");
+		bg.setBounds(0, 0, 618, 457);
+		contentPane.add(bg);
 	}
 }
