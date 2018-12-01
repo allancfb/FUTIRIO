c56d6c6808e275090488e17fb798967e7cc7fed9
Allan Borges <allancfborges@gmail.com>
2018-12-01 16:56:18 -0200

    Mudança nos pesos

--- src/entity/Posicao.java
+++ src/entity/Posicao.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public enum Posicao implements Serializable {
 
-	GOL("Gol", 7, 1, 1, 1, 90), DEFESA("Defesa", 10, 5, 35, 45, 5), MEIA("Meia", 25, 20, 25, 25, 5),
+	GOL("Gol", 7, 1, 1, 1, 90), DEFESA("Defesa", 5, 5, 35, 50, 5), MEIA("Meia", 20, 20, 40, 15, 5),
 	ATAQUE("Ataque", 50, 30, 10, 5, 5), DEFAULT("Reserva", 20, 20, 20, 20, 20);
 
 	private String nome;
