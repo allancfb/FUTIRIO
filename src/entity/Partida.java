2ac2c9ecf26c10964cc0726008ffea210c2e8d8d
Allan Borges <allancfborges@gmail.com>
2018-11-30 22:31:17 -0200

    Ajuste na chance de fazer gol

--- src/entity/Partida.java
+++ src/entity/Partida.java
@@ -93,11 +93,11 @@ public class Partida {
 		double sorteioPosse = r.nextDouble() * 100;
 
 		// Confere se algum time fez gol
-		if (sorteioPosse <= geral1Partida / 20) {
+		if (sorteioPosse <= geral1Partida / 25) {
 			time1Gols++;
 
 			atualizarPlacar();
-		} else if (sorteioPosse <= (geral2Partida / 20) + (geral1Partida / 18)) {
+		} else if (sorteioPosse <= (geral2Partida / 25) + (geral1Partida / 25)) {
 			time2Gols++;
 
 			atualizarPlacar();
